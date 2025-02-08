7d246df417690c14805dbdea1727796f10efd06e
saybz <shailash.bhati@gmail.com>
2025-02-08 22:02:48 +0100

    ADD: Header svg && Contact content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,11 +11,12 @@ export default async function Home() {
         <Image
           src="https://placehold.jp/2000x900.png"
           alt="Image"
-          fill
-          className=""
+          width={2000}
+          height={900}
+          className="w-screen h-[900px] object-cover"
         />
       </div>
-      <section id="about" className=" flex-col w-full max-w-main my-40">
+      <section id="about" className=" flex-col w-full max-w-main mb-40">
         <h2 className="relative before:absolute before:-bottom-4 before:left-0 before:rounded-sm before:w-20 before:h-[2px] before:bg-primary font-head font-light text-3xl underline-offset-2 mb-20">
           Restaurant Familial
         </h2>
@@ -27,7 +28,7 @@ export default async function Home() {
                 alt="Image"
                 width={700}
                 height={500}
-                className="rounded-lg shadow-lg"
+                className="rounded-lg shadow-lg text-primary "
               />
             </div>
           )}
@@ -38,19 +39,10 @@ export default async function Home() {
             <p className="text-lg text-gray-600">
               {content?.description || "Description par défaut"}
             </p>
-            {content?.pdf && (
-              <a
-                href={content.pdf}
-                target="_blank"
-                className="text-blue-500 underline"
-              >
-                Télécharger la carte
-              </a>
-            )}
           </div>
         </div>
       </section>
-      <section id="menu" className="max-w-main w-full my-40">
+      <section id="menu" className="max-w-main w-full mb-40">
         <h2 className="relative before:absolute before:-bottom-4 before:left-0 before:rounded-sm before:w-20 before:h-[2px] before:bg-primary font-head font-light text-3xl underline-offset-2 mb-20">
           Carte & Menus
         </h2>
@@ -79,10 +71,10 @@ export default async function Home() {
       </section>
       <section id="infos" className="w-screen py-40 bg-slate-200">
         <div className="max-w-main w-full mx-auto">
-          <h2 className="relative before:absolute before:-bottom-4 before:left-0 before:rounded-sm before:w-20 before:h-[2px] before:bg-primary font-head font-light text-3xl underline-offset-2 mb-20">
+          <h2 className="mx-10 relative before:absolute before:-bottom-4 before:left-0 before:rounded-sm before:w-20 before:h-[2px] before:bg-primary font-head font-light text-3xl underline-offset-2 mb-20">
             Infos pratiques
           </h2>
-          <div className="w-full flex justify-between mb-8">
+          <div className="w-full flex justify-between mb-8 px-10">
             <div className="flex-col">
               <h3 className="font-bold text-xl mb-2">Cuisine</h3>
               <p>Indienne, pakistanaise</p>
@@ -105,15 +97,63 @@ export default async function Home() {
         <h2 className="relative before:absolute before:-bottom-4 before:left-0 before:rounded-sm before:w-20 before:h-[2px] before:bg-primary font-head font-light text-3xl underline-offset-2 mb-20">
           Contact
         </h2>
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5401.635074206616!2d0.689015977602613!3d47.395992871171224!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47fcd5b248f1490b%3A0xd4bc494e1971897e!2sLe%20Palais%20du%20Rajah%20(Rajasthan)!5e0!3m2!1sfr!2sfr!4v1738936968444!5m2!1sfr!2sfr"
-          width="600"
-          height="450"
-          style={{ border: 0 }}
-          title="Google Maps location of the restaurant"
-          loading="lazy"
-          className="w-full h-[450px] rounded-lg shadow-lg"
-        ></iframe>
+        <div className="flex">
+          <div className="1/3 p-16 bg-white rounded">
+            <h3 className="font-bold font-head mb-8">Adresse :</h3>
+            <p className="py-5 mb-8 px-8 rounded whitespace-nowrap bg-primary text-white">
+              113 rue Colbert, 37000 Tours
+            </p>
+            <ul className="pl-3 border-l flex-col border-primary">
+              <li className="mb-3">A 8 min de la gare</li>
+              <li className="mb-3">200m de la Loire</li>
+              <li className="mb-3">300m de la cathédrale Saint gatien</li>
+              <li className="mb-3">Débouche sur la rue Nationale</li>
+            </ul>
+          </div>
+          <div className="w-full">
+            <div className="flex justify-end items-center mb-10">
+              <a
+                href="#contact"
+                className="relative p-3 pl-8 ml-8 bg-white text-black shadow-lg rounded-lg flex items-center hover:text-gray-900"
+              >
+                <div className="absolute -left-4 p-2 bg-white shadow-lg rounded-md mr-2">
+                  <Image
+                    src="/img/phone_icon.svg"
+                    alt="Phone icon"
+                    width={20}
+                    height={20}
+                    className="color-primary text-primary"
+                  />
+                </div>
+                palaisduraja@gmail.com
+              </a>
+              <a
+                href="#contact"
+                className="relative p-3 pl-8 ml-8 bg-white text-black shadow-lg rounded-lg flex items-center hover:text-gray-900"
+              >
+                <div className="absolute -left-4 p-2 bg-white shadow-lg rounded-md mr-2">
+                  <Image
+                    src="/img/phone_icon.svg"
+                    alt="Phone icon"
+                    width={20}
+                    height={20}
+                    className="color-primary text-primary"
+                  />
+                </div>
+                Reserver
+              </a>
+            </div>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5401.635074206616!2d0.689015977602613!3d47.395992871171224!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47fcd5b248f1490b%3A0xd4bc494e1971897e!2sLe%20Palais%20du%20Rajah%20(Rajasthan)!5e0!3m2!1sfr!2sfr!4v1738936968444!5m2!1sfr!2sfr"
+              width="600"
+              height="450"
+              style={{ border: 0 }}
+              title="Google Maps location of the restaurant"
+              loading="lazy"
+              className="w-full h-[450px] rounded-lg shadow-lg"
+            ></iframe>
+          </div>
+        </div>
       </section>
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,34 @@
 import React from "react";
+import Image from "next/image";
 
 const Header: React.FC = () => {
   return (
-    <header className="z-10 fixed w-screen top-0 left-0 bg-primary shadow-md h-24 text-white py-6">
+    <header className="z-10 fixed w-screen top-0 left-0 bg-primary shadow-md h-24 font-body text-white py-6">
       <div className="container h-full max-w-main mx-auto px-4 flex justify-between items-center">
         <div className="text-xl font-bold">Logo</div>
-        <nav className="flex space-x-4">
-          <a href="#about" className=" hover:text-gray-900">
+        <nav className="flex items-center ">
+          <a href="#about" className="mx-2 hover:text-gray-900">
             Qui sommes-nous ?
           </a>
-          <a href="#menu" className=" hover:text-gray-900">
+          <a href="#menu" className="mx-2 hover:text-gray-900">
             Menu
           </a>
-          <a href="#infos" className=" hover:text-gray-900">
+          <a href="#infos" className="mx-2 hover:text-gray-900">
             Infos
           </a>
-          <a href="#contact" className=" hover:text-gray-900">
+          <a
+            href="#contact"
+            className="relative p-3 pl-8 ml-8 bg-white text-black shadow-lg rounded-lg flex items-center hover:text-gray-900"
+          >
+            <div className="absolute -left-4 p-2 bg-white shadow-lg rounded-md mr-2">
+              <Image
+                src="/img/phone_icon.svg"
+                alt="Phone icon"
+                width={20}
+                height={20}
+                className="color-primary text-primary"
+              />
+            </div>
             Contact
           </a>
         </nav>
