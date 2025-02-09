3f2c09209e0b610295871db252a464ea7c2f11fe
saybz <shailash.bhati@gmail.com>
2025-02-09 12:21:46 +0100

    ADD: responsive base

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,33 +6,38 @@ export default async function Home() {
   const content = await prisma.content.findFirst();
 
   return (
-    <div className="min-h-screen w-screen px-10 font-body bg-gray-50 flex flex-col items-center">
-      <div className="relative w-screen max-h-screen mb-40">
+    <div className="min-h-screen w-screen px-10 font-body bg-gray-50 flex flex-col mt-24 items-center">
+      <div className="relative w-screen max-h-screen mb-20 md:mb-30 lg:mb-40">
         <Image
           src="https://placehold.jp/2000x900.png"
           alt="Image"
+          layout="responsive"
           width={2000}
           height={900}
-          className="w-screen h-[900px] object-cover"
+          className="w-screen object-cover"
         />
       </div>
-      <section id="about" className=" flex-col w-full max-w-main mb-40">
+      <section
+        id="about"
+        className="flex flex-col w-full max-w-main mb-20 md:mb-30 lg:mb-40"
+      >
         <h2 className="relative before:absolute before:-bottom-4 before:left-0 before:rounded-sm before:w-20 before:h-[2px] before:bg-primary font-head font-light text-3xl underline-offset-2 mb-20">
           Restaurant Familial
         </h2>
-        <div className="flex items-center">
+        <div className="flex flex-col lg:flex-row ">
           {content?.image && (
-            <div className="w-1/2">
+            <div className="w-full lg:w-1/2">
               <Image
                 src={content.image}
                 alt="Image"
+                layout="responsive"
                 width={700}
                 height={500}
                 className="rounded-lg shadow-lg text-primary "
               />
             </div>
           )}
-          <div className="ml-10">
+          <div className="ml-0 lg:ml-10 mt-10 lg:mt-0">
             <h2 className="text-4xl font-bold font-head mb-4">
               {content?.title || "Titre par défaut"}
             </h2>
@@ -42,16 +47,18 @@ export default async function Home() {
           </div>
         </div>
       </section>
-      <section id="menu" className="max-w-main w-full mb-40">
+      <section id="menu" className="max-w-main w-full mb-20 md:mb-30 lg:mb-40">
         <h2 className="relative before:absolute before:-bottom-4 before:left-0 before:rounded-sm before:w-20 before:h-[2px] before:bg-primary font-head font-light text-3xl underline-offset-2 mb-20">
           Carte & Menus
         </h2>
-        <div className="relative w-full h-[720px] justify-center items-center">
+        <div className="relative w-full justify-center items-center">
           {content?.image && (
             <Image
               src="https://placehold.jp/1440x720.png"
               alt="Image"
-              fill
+              layout="responsive"
+              width={1440}
+              height={720}
               className="relative rounded-lg shadow-lg"
             />
           )}
@@ -69,77 +76,86 @@ export default async function Home() {
           )}
         </div>
       </section>
-      <section id="infos" className="w-screen py-40 bg-slate-200">
+      <section
+        id="infos"
+        className="w-screen py-20 md:py-30 lg:py-40 bg-slate-200"
+      >
         <div className="max-w-main w-full mx-auto">
           <h2 className="mx-10 relative before:absolute before:-bottom-4 before:left-0 before:rounded-sm before:w-20 before:h-[2px] before:bg-primary font-head font-light text-3xl underline-offset-2 mb-20">
             Infos pratiques
           </h2>
-          <div className="w-full flex justify-between mb-8 px-10">
-            <div className="flex-col">
-              <h3 className="font-bold text-xl mb-2">Cuisine</h3>
-              <p>Indienne, pakistanaise</p>
+          <div className="w-full flex flex-col md:flex-row justify-between mb-8 px-10">
+            <div className="w-full md:w-1/2 flex-col pr-0 md:pr-10 mb-10 md:mb-0">
+              <div className="flex-col mb-10">
+                <h3 className="font-bold text-xl mb-2">Cuisine</h3>
+                <p>Indienne, pakistanaise</p>
+              </div>
+              <div className="flex-col">
+                <h3 className="font-bold text-xl mb-2">Moyens de paiement</h3>
+                <p>
+                  Carte Bleue, Visa, Eurocard/Mastercard, Paiement Sans Contact,
+                  Chèque vacances, Tickets restaurant
+                </p>
+              </div>
             </div>
-            <div className="flex-col px-10">
-              <h3 className="font-bold text-xl mb-2">Moyens de paiement</h3>
-              <p>
-                Carte Bleue, Chèques, Visa, Eurocard/Mastercard, Paiement Sans
-                Contact, Chèque vacances, Tickets restaurant
-              </p>
-            </div>
-            <div className=" flex-col">
-              <h3 className="font-bold text-xl mb-2">Horaires</h3>
-              <p>Du mercredi 19h au lundi</p>
+            <div className="flex-col w-full md:w-1/2 p-10 border border-primary">
+              <h3 className="font-bold text-xl mb-6">Horaires :</h3>
+              <div className="grid grid-cols-2 gap-4">
+                <span>Mardi - Mercredi midi</span>
+                <span>Fermé</span>
+                <span>Mercredi soir - lundi</span>
+                <span>12h00-14h30 19h00-22h30</span>
+              </div>
             </div>
           </div>
         </div>
       </section>
-      <section id="contact" className="max-w-main w-full my-40">
+      <section
+        id="contact"
+        className="max-w-main w-full my-20 md:mb-30 lg:mb-40"
+      >
         <h2 className="relative before:absolute before:-bottom-4 before:left-0 before:rounded-sm before:w-20 before:h-[2px] before:bg-primary font-head font-light text-3xl underline-offset-2 mb-20">
           Contact
         </h2>
-        <div className="flex">
-          <div className="1/3 p-16 bg-white rounded">
+        <div className="flex flex-col items-center lg:flex-row">
+          <div className="w-full p-16 bg-white rounded shadow-lg mb-10 lg:mb-0 h-full lg:max-w-[500px]">
             <h3 className="font-bold font-head mb-8">Adresse :</h3>
             <p className="py-5 mb-8 px-8 rounded whitespace-nowrap bg-primary text-white">
               113 rue Colbert, 37000 Tours
             </p>
             <ul className="pl-3 border-l flex-col border-primary">
-              <li className="mb-3">A 8 min de la gare</li>
-              <li className="mb-3">200m de la Loire</li>
-              <li className="mb-3">300m de la cathédrale Saint gatien</li>
-              <li className="mb-3">Débouche sur la rue Nationale</li>
+              <li className="mb-6">A 8 min de la gare</li>
+              <li className="mb-6">200m de la Loire</li>
+              <li className="mb-6">300m de la cathédrale Saint gatien</li>
+              <li className="mb-6">Débouche sur la rue Nationale</li>
             </ul>
           </div>
           <div className="w-full">
-            <div className="flex justify-end items-center mb-10">
+            <div className="flex flex-col md:flex-row lg:justify-end justify-around items-center mb-10">
               <a
                 href="#contact"
-                className="relative p-3 pl-8 ml-8 bg-white text-black shadow-lg rounded-lg flex items-center hover:text-gray-900"
+                className="relative p-3 pl-8 ml-0 md:ml-8 mb-6 md:mb-0 bg-white text-black shadow-lg rounded-lg flex items-center hover:text-gray-900"
               >
-                <div className="absolute -left-4 p-2 bg-white shadow-lg rounded-md mr-2">
-                  <Image
-                    src="/img/phone_icon.svg"
-                    alt="Phone icon"
-                    width={20}
-                    height={20}
-                    className="color-primary text-primary"
-                  />
-                </div>
+                <Image
+                  src="/img/mail.svg"
+                  alt="mail icon"
+                  width={32}
+                  height={32}
+                  className="absolute -left-4 shadow-lg mr-2"
+                />
                 palaisduraja@gmail.com
               </a>
               <a
                 href="#contact"
-                className="relative p-3 pl-8 ml-8 bg-white text-black shadow-lg rounded-lg flex items-center hover:text-gray-900"
+                className="relative p-3 pl-8 ml-0 md:ml-8 bg-white text-black shadow-lg rounded-lg flex items-center hover:text-gray-900"
               >
-                <div className="absolute -left-4 p-2 bg-white shadow-lg rounded-md mr-2">
-                  <Image
-                    src="/img/phone_icon.svg"
-                    alt="Phone icon"
-                    width={20}
-                    height={20}
-                    className="color-primary text-primary"
-                  />
-                </div>
+                <Image
+                  src="/img/phone.svg"
+                  alt="phone icon"
+                  width={32}
+                  height={32}
+                  className="absolute -left-4 shadow-lg mr-2"
+                />
                 Reserver
               </a>
             </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header: React.FC = () => {
   return (
     <header className="z-10 fixed w-screen top-0 left-0 bg-primary shadow-md h-24 font-body text-white py-6">
       <div className="container h-full max-w-main mx-auto px-4 flex justify-between items-center">
-        <div className="text-xl font-bold">Logo</div>
+        <div className="text-xl font-bold">Palais du Raja</div>
         <nav className="flex items-center ">
           <a href="#about" className="mx-2 hover:text-gray-900">
             Qui sommes-nous ?
@@ -18,17 +18,15 @@ const Header: React.FC = () => {
           </a>
           <a
             href="#contact"
-            className="relative p-3 pl-8 ml-8 bg-white text-black shadow-lg rounded-lg flex items-center hover:text-gray-900"
+            className="relative py-3 pl-8 pr-4 ml-8 bg-white text-black shadow-lg rounded-lg flex items-center hover:text-gray-900"
           >
-            <div className="absolute -left-4 p-2 bg-white shadow-lg rounded-md mr-2">
-              <Image
-                src="/img/phone_icon.svg"
-                alt="Phone icon"
-                width={20}
-                height={20}
-                className="color-primary text-primary"
-              />
-            </div>
+            <Image
+              src="/img/phone.svg"
+              alt="phone icon"
+              width={32}
+              height={32}
+              className="absolute -left-4 shadow-lg mr-2"
+            />
             Contact
           </a>
         </nav>
